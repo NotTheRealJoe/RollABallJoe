a87ec7da9850a8020a0a73a31ba3416ea273bbba
Paul D <pdemange19@cornellcollege.edu>
2018-03-21 14:44:45 -0500

    Added Gud Stuff
    -Added skin to player.
    -Added skin to background.
    -Added music component.

---
+++ Assets/Scripts/AudioBoi.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBoi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioSource audio = GetComponent<AudioSource> (); 
+		audio.Play ();
+	}
+
+}
